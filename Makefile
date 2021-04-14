0f386d54244dd4325a21d1d28fdf8a4a8f7276de
Nathan Wehr <nathan@wehrholdings.com>
2021-04-14 18:42:52 -0400

    Compatable with C++ comiler

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
-CC     = cc
+CC     = c++
 CFLAGS = -Wall -O2
 LDLIBS = -lsodium
 
-test:
-	$(CC) $(CFLAGS) -o test test.c $(LDLIBS)
+server:
+	$(CC) $(CFLAGS) -o server server.cpp mud.cpp aegis256.c  $(LDLIBS)
+
+client:
+	$(CC) $(CFLAGS) -o client client.cpp mud.cpp aegis256/aegis256.c $(LDLIBS)
 
 clean:
 	rm -f test
--- mud.h
+++ mud.h
@@ -5,8 +5,41 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#include <sodium.h>
+
+#if defined __APPLE__
+#include <mach/mach_time.h>
+#endif
+
 #define MUD_PATH_MAX    (32U)
 #define MUD_PUBKEY_SIZE (32U)
+
+#define MUD_ONE_MSEC (UINT64_C(1000))
+#define MUD_ONE_SEC  (1000 * MUD_ONE_MSEC)
+#define MUD_ONE_MIN  (60 * MUD_ONE_SEC)
+
+#define MUD_TIME_SIZE    (6U)
+#define MUD_TIME_BITS    (MUD_TIME_SIZE * 8U)
+#define MUD_TIME_MASK(X) ((X) & ((UINT64_C(1) << MUD_TIME_BITS) - 2))
+
+#define MUD_KEY_SIZE (32U)
+#define MUD_MAC_SIZE (16U)
+
+#define MUD_MSG(X)       ((X) & UINT64_C(1))
+#define MUD_MSG_MARK(X)  ((X) | UINT64_C(1))
+#define MUD_MSG_SENT_MAX (5)
+
+#define MUD_PKT_MIN_SIZE (MUD_TIME_SIZE + MUD_MAC_SIZE)
+#define MUD_PKT_MAX_SIZE (1500U)
+
+#define MUD_MTU_MIN ( 576U + MUD_PKT_MIN_SIZE)
+#define MUD_MTU_MAX (1450U + MUD_PKT_MIN_SIZE)
+
+#define MUD_CTRL_SIZE (CMSG_SPACE(MUD_PKTINFO_SIZE) + \
+                       CMSG_SPACE(sizeof(in6_pktinfo)))
+
+#define MUD_STORE_MSG(D,S) mud_store((D),(S),sizeof(D))
+#define MUD_LOAD_MSG(S)    mud_load((S),sizeof(S))
 
 struct mud;
 
@@ -109,20 +142,91 @@ struct mud_paths {
     unsigned count;
 };
 
-struct mud *mud_create (union mud_sockaddr *, unsigned char *, int *);
-void        mud_delete (struct mud *);
+struct mud_crypto_opt {
+    unsigned char* dst;
+    const unsigned char* src;
+    size_t size;
+};
 
-int mud_set      (struct mud *, struct mud_conf *);
-int mud_set_path (struct mud *, struct mud_path_conf *);
+struct mud_addr {
+    union {
+        unsigned char v6[16];
+        struct {
+            unsigned char zero[10];
+            unsigned char ff[2];
+            unsigned char v4[4];
+        };
+    };
+    unsigned char port[2];
+};
 
-int mud_update    (struct mud *);
-int mud_send_wait (struct mud *);
+struct mud_msg {
+    unsigned char sent_time[MUD_TIME_SIZE];
+    unsigned char aes;
+    unsigned char pkey[MUD_PUBKEY_SIZE];
+    struct {
+        unsigned char bytes[sizeof(uint64_t)];
+        unsigned char total[sizeof(uint64_t)];
+    } tx, rx, fw;
+    unsigned char max_rate[sizeof(uint64_t)];
+    unsigned char beat[MUD_TIME_SIZE];
+    unsigned char mtu[2];
+    unsigned char pref;
+    unsigned char loss;
+    unsigned char fixed_rate;
+    unsigned char loss_limit;
+    struct mud_addr addr;
+};
 
-int mud_recv (struct mud *, void *, size_t);
-int mud_send (struct mud *, const void *, size_t);
+struct mud_crypto_key {
+    struct {
+        unsigned char key[MUD_KEY_SIZE];
+    } encrypt, decrypt;
+    int aes;
+};
 
-int    mud_get_errors (struct mud *, struct mud_errors *);
-int    mud_get_fd     (struct mud *);
-size_t mud_get_mtu    (struct mud *);
-int    mud_get_paths  (struct mud *, struct mud_paths *,
-                       union mud_sockaddr *, union mud_sockaddr *);
+struct mud_keyx_t {
+    uint64_t time;
+    unsigned char secret[crypto_scalarmult_SCALARBYTES];
+    unsigned char remote[MUD_PUBKEY_SIZE];
+    unsigned char local[MUD_PUBKEY_SIZE];
+    mud_crypto_key priv, last, next, current;
+    int use_next;
+    int aes;
+};
+
+struct mud {
+    int fd;
+    mud_conf conf;
+    mud_path* paths;
+    unsigned pref;
+    unsigned capacity;
+    mud_keyx_t keyx;
+    uint64_t last_recv_time;
+    size_t mtu;
+    mud_errors err;
+    uint64_t rate;
+    uint64_t window;
+    uint64_t window_time;
+    uint64_t base_time;
+#if defined __APPLE__
+    mach_timebase_info_data_t mtid;
+#endif
+};
+
+mud* mud_create (union mud_sockaddr*, unsigned char*, int*);
+void mud_delete (mud*);
+
+int mud_set(mud*, mud_conf*);
+int mud_set_path(mud*, mud_path_conf*);
+
+int mud_update(mud*);
+int mud_send_wait(mud*);
+
+int mud_recv(mud*, void*, size_t);
+int mud_send(mud*, const void*, size_t);
+
+int mud_get_errors(mud*, mud_errors*);
+int mud_get_fd(mud*);
+size_t mud_get_mtu(mud*);
+int mud_get_paths(mud*, mud_paths*, union mud_sockaddr *, union mud_sockaddr *);
